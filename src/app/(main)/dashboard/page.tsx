97993a08e46ca1fb5b704e66fbc32989a5b20a0d
izuchukwu-eric <onukwubeizu@gmail.com>
2023-12-31 12:05:51 +0100

    working on creating workspace in dashboard

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,9 +1,39 @@
+import DashboardSetup from '@/components/dashboard-setup/dashboard-setup';
+import db from '@/lib/supabase/db';
+import { getUserSubscriptionStatus } from '@/lib/supabase/queries';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const Page = () => {
+const DashboardPage = async () => {
+    const supabase = createServerComponentClient({ cookies });
+
+    const { data: {user} } = await supabase.auth.getUser();
+
+    if(!user) return;
+
+    const workspace = await db.query.workspaces.findFirst({
+        where: (workspace, { eq }) => eq(workspace.workspaceOwner, user.id)
+    })
+
+    const { data: subscription, error: subscriptionError } = await getUserSubscriptionStatus(user.id)
+
+    if(subscriptionError) return;
+
+    if(!workspace) return (
+        <div className='bg-background h-screen w-screen flex justify-center items-center'>
+            <DashboardSetup user={user} subscription={subscription}>
+
+            </DashboardSetup>
+        </div>
+    )
+
+    redirect(`/dashboard/${workspace.id}`);
+
   return (
     <div>DashboardPage</div>
   )
 }
 
-export default Page
+export default DashboardPage
---
+++ src/components/dashboard-setup/dashboard-setup.tsx
@@ -0,0 +1,88 @@
+"use client"
+import { AuthUser } from '@supabase/supabase-js'
+import React, { useState } from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card';
+import EmojiPicker from '../global/emoji-picker';
+import { Label } from '../ui/label';
+import { Input } from '../ui/input';
+import { FieldValues, useForm } from 'react-hook-form';
+import { Subscription } from '@/lib/supabase/supabase.types';
+
+interface DashboardSetupProps {
+    user: AuthUser;
+    subscription: Subscription | null
+}
+
+const DashboardSetup: React.FC<DashboardSetupProps> = ({ user, subscription }) => {
+    const [selectedEmoji, setSelectedEmoji] = useState("💼");
+    const { register, handleSubmit, reset, formState:{ isSubmitting: isLoading, errors } } = useForm<FieldValues>({
+        mode: "onChange",
+        defaultValues: {
+            logo: '',
+            workspaceName: ''
+        }
+    })
+  return (
+    <Card className='w-[800px] h-screen sm:h-auto'>
+        <CardHeader>
+            <CardTitle>
+                Create a Workspace
+            </CardTitle>
+            <CardDescription>
+                Lets create a private workspace to get you started.You can add
+                collaborators later from the workspace settings tab.
+            </CardDescription>
+        </CardHeader>
+        <CardContent>
+            <form onSubmit={() => {}}>
+                <div className='flex flex-col gap-4'>
+                    <div className='flex items-center gap-4'>
+                        <div className='text-5xl'>
+                            <EmojiPicker getValue={(emoji) => setSelectedEmoji(emoji)}>
+                                {selectedEmoji}
+                            </EmojiPicker>
+                        </div>
+                        <div className='w-full'>
+                            <Label htmlFor='workspaceName' className='text-sm text-muted-foreground'>
+                                Name
+                            </Label>
+                            <Input 
+                                id='workspaceName' 
+                                type='text'
+                                placeholder='Workspace Name'
+                                disabled={isLoading}
+                                {...register('workspaceName', {
+                                    required: 'Workspace name is required'
+                                })}
+                            />
+                            <small className='text-red-600'>
+                                {errors?.workspaceName?.message?.toString()}
+                            </small>
+                        </div>
+                    </div>
+                    <div>
+                        <Label htmlFor='logo' className='text-sm text-muted-foreground'>
+                            Workspace Logo
+                        </Label>
+                        <Input 
+                            id='logo' 
+                            type='file'
+                            accept='image/*'
+                            placeholder='Workspace Logo'
+                            disabled={isLoading || subscription?.status !== 'active'}
+                            {...register('logo', {
+                                required: 'Workspace name is required'
+                            })}
+                        />
+                        <small className='text-red-600'>
+                            {errors?.logo?.message?.toString()}
+                        </small>
+                    </div>
+                </div>
+            </form>
+        </CardContent>
+    </Card>
+  )
+}
+
+export default DashboardSetup
